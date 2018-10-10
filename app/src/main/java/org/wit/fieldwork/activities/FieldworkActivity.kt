db0363bfa5e4ab6428591575a6e31d8c856b2e71
Judy OBrien <judyobrienie@gmail.com>
2018-10-10 20:21:13 +0100

    Adding Array of fieldworks and Description Field

--- app/src/main/java/org/wit/fieldwork/activities/FieldworkActivity.kt
+++ app/src/main/java/org/wit/fieldwork/activities/FieldworkActivity.kt
@@ -1,4 +1,4 @@
-package org.wit.fieldwork
+package org.wit.fieldwork.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -6,19 +6,26 @@ import kotlinx.android.synthetic.main.activity_fieldwork.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
+import org.wit.fieldwork.R
+import org.wit.fieldwork.models.FieldworkModel
 
 
 class FieldworkActivity : AppCompatActivity(), AnkoLogger {
 
+  val fieldworks = ArrayList<FieldworkModel>()
+  var fieldwork = FieldworkModel()
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_fieldwork)
-    info("Placemark Activity started..")
+    info("Fieldwork Activity started..")
 
     btnAdd.setOnClickListener() {
-      val placemarkTitle = fieldworkTitle.text.toString()
-      if (placemarkTitle.isNotEmpty()) {
-        info("add Button Pressed: $placemarkTitle")
+      fieldwork.title = fieldworkTitle.text.toString()
+      fieldwork.description = fieldworkDescription.text.toString()
+      if (fieldwork.title.isNotEmpty() && fieldwork.description.isNotEmpty()) {
+        info("array: $fieldworks")
+        fieldworks.add(fieldwork.copy())
       }
       else {
         toast ("Please Enter a title")
